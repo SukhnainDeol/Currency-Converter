067096f147d61a139d3cf20aadd21bcd40da0102
Sukhnain Deol <dsukhnain@gmail.com>
2023-10-12 03:48:18 -0700

    testing & commenting completed

--- currencyConverter.js
+++ currencyConverter.js
@@ -1,13 +1,17 @@
-// Name: Sukhnain Deol
-// Purpose:
+/** Name: Sukhnain Deol
+ *  Purpose: This program prompts the user for a currency code to convert, the amount to convert,
+ *           and the currency code to convert to. Then it alerts the user the resulting conversion.
+ */
 
-// tODO;
-    // integrate with existing code
-    // make css for buttons nicer
-    // currncy search.js
 
+
+/** 
+ * prompts user for currency code, amount to convert, and converted
+ * currency code then alerts thems with the converted value
+ */
 function promptConvertCurrency() 
 {
+    let date = "2022-09-24";
     let rates = 
     { 
         "AUD": 1.531863,
@@ -23,10 +27,6 @@ function promptConvertCurrency()
         "ZAR": 17.92624
     };
 
-    let date = "2022-09-24";
-
-
-    
     // ask user for original currency's code
     let questionOne = "Hello, Welcome to the Currency Converter!\nWhat currency would you like to convert? (3-digit code)";
     let currencyCodeError = "Please enter a valid currency code, use the currency searcher to help.";
@@ -34,41 +34,52 @@ function promptConvertCurrency()
 
     // ask user for amount to convert
     let questionTwo = "How much do you want to convert?";
-    let currencyAmount = askForNumber(questionTwo);
+    let amount = askForNumber(questionTwo);
     
     // ask user for currency code to convert to
     let questionThree = "What currency do you want it converted to? (3-digit code)";
     let convertedCurrency = askForString(questionThree, rates, currencyCodeError);
 
 
-    // calculate value & round to 2 decimal
-    let convertedValue = convertCurrency(currentCurrency, convertedCurrency, currencyAmount, rates);
+    // multiplies amount by a fraction of current and converted currrencies' values & round to 2 decimal
+    let convertedValue = (amount * (rates[convertedCurrency]/rates[currentCurrency])).toFixed(2);
 
-    // alert conversion value to user
-    alert(currencyAmount + " " + currentCurrency + " is " + convertedValue + " " + convertedCurrency + ". As of " + date + ".");
-}
-
-
-function convertCurrency(current, converted, amount, rates)
-{
-    return (amount * (rates[converted]/rates[current])).toFixed(2);
+    // alert conversion value to user (e.g. 100 USD is 153.19 AUD. As of 2022-09-24)
+    alert(amount + " " + currentCurrency + " is " + convertedValue + " " + convertedCurrency + ". As of " + date + ".");
 }
 
 
 
-function askForString(question, answerJson, errorAnswer)
+/** 
+ * prompts user a question and alerts a provided error if the input is not 
+ * from the provided answers and repeats, then returns onces a valid string is entered
+ * 
+ * @param {string} question - question to prompt
+ * @param {object} answers - valid answers to prompt
+ * @param {string} errorAnswer - error to alert for invalid input
+ * 
+ * @returns - user input if it is valid
+ */
+function askForString(question, answers, errorAnswer)
 {
     do // ask question, give error answer if incorrect
     { 
         var answer = prompt(question);
-        if (!(answer.toUpperCase() in answerJson)) {alert(errorAnswer);}
+        if (!(answer.toUpperCase() in answers)) {alert(errorAnswer);}
     } 
-    while (!(answer.toUpperCase() in answerJson));
+    while (!(answer.toUpperCase() in answers));
 
     return answer.toUpperCase();
 }
 
 
+/** 
+ * prompts user a question until they input a number
+ * 
+ * @param {string} question - question to prompt
+ * 
+ * @returns - inputted number from user 
+ */
 function askForNumber(question)
 {
     do // ask question, give error answer if incorrect
--- currencySearcher.js
+++ currencySearcher.js
@@ -1,9 +1,21 @@
-// Name: Sukhnain Deol
-// Purpose:
+/**
+ *  Name: Sukhnain Deol
+ *  Purpose: This program prompts a user for a partial string of a currency code then 
+ *          searches a JSON of currency codes keys and associated full name 
+ *          to alert the user of currencies that have that partial string as a substring
+ */          
 
 
+
+/** 
+ *  prompts the user for a search string and returns any key or value
+ *  in symbols that have that value as a substring
+ * 
+ * Example: user enters 'aud' in prompt, function alerts AUD: Australian Dollar
+ */
 function promptSearchCurrency()
 {
+    // currency codes & associated values
     let symbols = 
     {
         "AUD": "Australian Dollar",
@@ -19,18 +31,26 @@ function promptSearchCurrency()
         "ZAR": "South African Rand"
     };
 
+    // get searchString and use it to search symbols then alert user of results
     let searchString = prompt("Hello, Welcome to the Currency Searcher!\nPlease enter a part of the currency's name.").toLowerCase();
-
     let searchResults = searchCurrency(searchString, symbols);
-
     alertFormattedSymbols(searchResults);
 }
 
 
+/** 
+ * searches objects for keys of values that match searchString and return 
+ * them in an array
+ * 
+ * @param {string} searchString - string given by user to search
+ * @param {object} symbols - object of currency codes and names
+ * 
+ * @returns {array} - array of valid keys-value pairs (key: Value)
+ */
 function searchCurrency(searchString, symbols)
 {
     let searchResults = [];
-
+    // search each code, name object and add it to arr if it includes partial string
     for (let [key, value] of Object.entries(symbols))
     {
         if (key.toLowerCase().includes(searchString) || value.toLowerCase().includes(searchString)) 
@@ -38,18 +58,25 @@ function searchCurrency(searchString, symbols)
             searchResults.push(key + ": " + value);
         }
     }
-
     return searchResults;
 }
 
 
+/** 
+ * Formats elements in arr to each on their own line and alerts
+ * them as search result, if arr is empty, alert as such
+ * 
+ * @param {array} arr - array of values to add to alertValue
+ * 
+ * Returns
+ */
 function alertFormattedSymbols(arr)
 {
+    // format each arr element on its own line 
     let alertValue = "";
-    for(let i = 0; i < arr.length; i++)
-    {
-        alertValue += arr[i] + "\n";
-    }
+    for(let i = 0; i < arr.length; i++) {alertValue += arr[i] + "\n";}
+
+    // if no results, alert as such
     if (alertValue.length === 0) {alert("No currencies contain that string.");}
     else {alert("Search Results:\n" + alertValue);}
 }
